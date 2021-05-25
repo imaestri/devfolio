67ed4923990aa5e09fc9fcfc02dede64a217f552
Isaque Maestri <60708221+imaestri@users.noreply.github.com>
2021-05-25 10:13:47 -0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,8 +22,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Simplefolio`,
-        short_name: `Simplefolio`,
+        name: `Imaestri | Devfolio`,
+        short_name: `Imaestri`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#02aab0`,
